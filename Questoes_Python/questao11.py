09334d6d131fa457e965d6d95bfa9169cf9a4302
João Victor <joaovvictor.gg@gmail.com>
2026-03-22 19:53:57 -0300

    adiciona comentário explicativo no programa que solicita nome e sobrenome do usuário

--- Questoes_Python/questao11.py
+++ Questoes_Python/questao11.py
@@ -1,3 +1,5 @@
+# Questão 11 - Escreva um programa que solicite ao usuário o seu nome e sobrenome, e exiba uma mensagem personalizada.
+
 nome = input("Digite o seu nome: ")
 sobrenome = input("Digite o seu sobrenome: ")
 print("Olá, " + nome + " " + sobrenome + "! Seu nome é hórrivel!")
