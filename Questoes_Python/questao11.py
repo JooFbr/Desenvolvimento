25a7d91a6be4d9d719bb370b8fdfca8dc601600c
João Victor <joaovvictor.gg@gmail.com>
2026-03-22 22:12:51 -0300

    corrige a conversão de entrada para string no programa que solicita nome e sobrenome do usuário

--- Questoes_Python/questao11.py
+++ Questoes_Python/questao11.py
@@ -1,5 +1,5 @@
 # Questão 11 - Escreva um programa que solicite ao usuário o seu nome e sobrenome, e exiba uma mensagem personalizada.
 
-nome = input("Digite o seu nome: ")
-sobrenome = input("Digite o seu sobrenome: ")
+nome = str(input("Digite o seu nome: "))
+sobrenome = str(input("Digite o seu sobrenome: "))
 print("Olá, " + nome + " " + sobrenome + "! Seu nome é hórrivel!")
