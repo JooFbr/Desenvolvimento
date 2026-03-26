4ef84f78ce48b6623baea0ef966474bb9c8dbbc8
João Victor <joaovvictor.gg@gmail.com>
2026-03-25 22:57:02 -0300

    remove o arquivo questa48.py que exibia letras de uma palavra em linhas separadas

--- Questoes_Python/questa48.py
+++
@@ -1,8 +0,0 @@
-# 48. Escreva um algoritmo que solicite ao usuário uma palavra e exiba cada letra da palavra em uma linha separada.
-
-palavra = input("Digite uma palavra: ")
-print("As letras da palavra são:")
-
-for letra in palavra:
-    print(letra)
-    
